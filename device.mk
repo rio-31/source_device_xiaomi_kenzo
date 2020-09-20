dcca0d58b5d08289b91b4cc7c5250750f94fa6c7
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-09-19 19:01:19 -0700

    kenzo: ANT+: Removed Temporary

--- device.mk
+++ device.mk
@@ -116,8 +116,8 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+#PRODUCT_COPY_FILES += \
+#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
