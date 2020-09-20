aa148b209006e10bb42af34dbaa6044b7a1c1307
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-09-19 18:36:00 -0700

    Telephony-IMS:NUKED

--- device.mk
+++ device.mk
@@ -318,9 +318,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
-
 # IPA Manager
 #PRODUCT_PACKAGES += \
 #    ipacm \
@@ -438,9 +435,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Seccomp policy
