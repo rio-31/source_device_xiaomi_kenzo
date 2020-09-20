3fbf303c3704e471b1f481f0c11968a54f28a491
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-09-19 20:57:04 -0700

    kenzo: Adapt Minimal Sepolicy 	-Sepolicy Switched to Minimal 	-Updated Sepolicy Version to 30.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,9 +227,9 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib64/lib-imsvideocodec.so|libshim_ims.so
 
 # SELinux
-BOARD_SEPOLICY_VERS := 29.0
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_VERS := 30.0
+#include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
