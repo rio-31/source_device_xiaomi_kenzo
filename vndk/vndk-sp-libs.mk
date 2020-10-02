a0841aca52d2eb2f0b5a783804d6cb9e4a21da13
trkzmn <trkzmn89@gmail.com>
2020-10-02 05:45:19 +0000

    kenzo: vndk: Clean-up

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -2,9 +2,43 @@ VNDK_SP_LIBRARIES := \
     android.frameworks.bufferhub@1.0 \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.allocator@3.0 \
-    libbinder \
+    android.hardware.graphics.mapper@2.0 \
+    android.hardware.graphics.mapper@2.1 \
+    android.hardware.graphics.mapper@3.0 \
+    android.hardware.graphics.common@1.0 \
+    android.hardware.graphics.common@1.1 \
+    android.hardware.graphics.common@1.2 \
+    android.hardware.renderscript@1.0 \
+    android.hidl.memory@1.0 \
+    android.hidl.memory.token@1.0 \
+    android.hidl.safe_union@1.0 \
+    libRSCpuRef \
+    libRSDriver \
+    libRS_internal \
     libdexfile_support \
-    libui
+    libbacktrace \
+    libbase \
+    libbcinfo \
+    libblas \
+    libc++ \
+    libcompiler_rt \
+    libcutils \
+    libhardware \
+    libhidlbase \
+    libhidlmemory \
+    libhidltransport \
+    libhwbinder \
+    libion \
+    libjsoncpp \
+    liblzma \
+    libprocessgroup \
+    libunwind \
+    libunwindstack\
+    libutils \
+    libutilscallstack \
+    libbinder \
+    libui \
+    libz
 
 install_in_hw_dir := \
    android.hidl.memory@1.0-impl
