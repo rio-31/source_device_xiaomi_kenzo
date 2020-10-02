fd3c87c526428b22a37dce8024be3025425e25a2
trkzmn <trkzmn89@gmail.com>
2020-10-02 07:01:55 +0000

    kenzo: camera: Switch legacy passthrough camera hal * Also remove external camera config

--- device.mk
+++ device.mk
@@ -188,20 +188,13 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.4-external-service \
-    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     camera.msm8952 \
     libqomx_core \
     Snap \
-    SnapdragonCamera \
-    SnapdragonCamera2 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
-
-PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
