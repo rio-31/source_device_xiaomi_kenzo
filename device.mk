85064adb4165d3a3bdbc6b9a7d688557daf32997
mrjarvis698 <dudamajaykumar59@gmail.com>
2020-10-14 17:58:21 +0000

    Kenzo : Update Radio

--- device.mk
+++ device.mk
@@ -320,8 +320,8 @@ PRODUCT_PACKAGES += \
     libbase_shim \
     ims-ext-common \
     ims-ext-common_system \
-    android.hardware.radio@1.0 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0
 
 # IPA Manager
